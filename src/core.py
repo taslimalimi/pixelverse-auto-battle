ed52e2233e748a64b0ebd927c3592797559e2e3c
its jaw 🦈 <63976518+jawikas@users.noreply.github.com>
2024-06-28 22:04:59 +0700

    Update core.py
    fix some fuction : 
    
     - update upgrade_pet fuction
     - add argument (--upgrade)
     - add min & max hit speed configuration
     - update config.json
     - update balance after fight

--- src/core.py
+++ src/core.py
@@ -2,9 +2,11 @@ import asyncio
 import json
 import sys
 import os
+import argparse
+import random
 from src.Battle import Battle
-from src.Battle import countdown_timer, split_chunk,clear_screen
-from src.Battle import print_with_timestamp, merah,biru,kuning,hijau,hitam,putih
+from src.Battle import countdown_timer, split_chunk, clear_screen
+from src.Battle import print_with_timestamp, merah, biru, kuning, hijau, hitam, putih
 from src.api_pets import UserPixel
 from colorama import *
 from datetime import datetime
@@ -20,55 +22,47 @@ def print_banner():
     ██║   ██║   ███████╗     ██║███████║██║ █╗ ██║
     ██║   ██║   ╚════██║██   ██║██╔══██║██║███╗██║
     ██║   ██║   ███████║╚█████╔╝██║  ██║╚███╔███╔╝
-    ╚═╝   ╚═╝   ╚══════╝ ╚════╝ ╚═╝  ╚═╝ ╚══╝╚══╝  """ 
+    ╚═╝   ╚═╝   ╚══════╝ ╚════╝ ╚═╝  ╚═╝ ╚══╝╚══╝  """
     print(Fore.GREEN + Style.BRIGHT + banner + Style.RESET_ALL)
     print(hijau + "    Pixeltap by Pixelverse Auto Battle")
     print(merah + f"    NOT FOR SALE = Free to use")
-    print(merah + f"    before start please '{hijau}git pull{merah}' to update bot")
-    
+    print(merah + f"    Please {hijau}'git pull{merah}' to update this bot")
+
 async def update_user_info(user):
     users = user.getUsers()
     stats = user.getStats()
     winRate = (stats['wins'] / stats['battlesCount']) * 100 if stats['battlesCount'] > 0 else 0
 
     print(f"{hijau}{'~' * 62}\r")
-    print_with_timestamp(f"{kuning}User: {users['username']} | Balance: {split_chunk(str(int(users['clicksCount'])))}")
+    print_with_timestamp(f"{hijau}User: {putih}{users['username']} {kuning}| Balance: {putih}{split_chunk(str(int(users['clicksCount'])))}")
     print_with_timestamp(f"{hijau}W{kuning}/{merah}L {hijau}: "
-    f"{hijau}{split_chunk(str(stats['wins']))}{kuning}/{merah}{split_chunk(str(stats['loses']))}\t "
-    f"{kuning}| {hijau}Wins: {split_chunk(str(stats['winsReward']))}")
-    print_with_timestamp(f"{biru}Match: {split_chunk(str(stats['battlesCount']))}\t "
-    f"{kuning}| {merah}Loses: {split_chunk(str(stats['losesReward']))}")
-    print_with_timestamp(f"{hijau}Winrate: {putih}{winRate:.2f}%\t " 
-    f"{kuning}| {biru}Earned: {split_chunk(str(stats['winsReward'] + stats['losesReward']))}")
+                         f"{hijau}{split_chunk(str(stats['wins']))}{kuning}/{merah}{split_chunk(str(stats['loses']))}\t "
+                         f"{kuning}| {hijau}Wins: {putih}{split_chunk(str(stats['winsReward']))}")
+    print_with_timestamp(f"{hijau}Match: {putih}{split_chunk(str(stats['battlesCount']))}\t "
+                         f"{kuning}| {merah}Loses: {putih}{split_chunk(str(stats['losesReward']))}")
+    print_with_timestamp(f"{hijau}Winrate: {putih}{winRate:.2f}%\t "
+                         f"{kuning}| {biru}Earned: {putih}{split_chunk(str(stats['winsReward'] + stats['losesReward']))}")
+
 async def main():
+    arg = argparse.ArgumentParser()
+    arg.add_argument('--upgrade', help="Set upgrade_pets to True", action='store_true')
+    args = arg.parse_args()
     clear_screen()
     print_banner()
     try:
         init()
-        user = UserPixel()
-        
-        await update_user_info(user)
-        users = user.getUsers()
-        while True:
-            with open('./config.json', 'r') as config_file:
-                config = json.load(config_file)
-
-            battle = Battle()
+        with open(f"./config.json", 'r') as config_file:config = json.load(config_file)
+        if args.upgrade: config['upgrade_pets'] = True
+        user = UserPixel(config)
+        await update_user_info(user)     
+        while True:            
+            battle = Battle(config)
             await battle.connect()
             del battle
 
             user.upgradePets(upgrade_pets=config['upgrade_pets'])
-            print_with_timestamp(f"{hijau}Balance : {putih}{split_chunk(str(int(users['clicksCount'])))}")
-            print_with_timestamp(f"{hitam}{'~' * 42}\r")
-            
             countdown_timer(15)
             await asyncio.sleep(1)
+  
     except Exception as e:
         print(e)
-
-if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        print_with_timestamp(f"{merah}Program terminated by user.")
-        sys.exit()
